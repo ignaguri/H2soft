12bac760dacd20d1bfc97e1893aecd012d9ffb11
Ignacio Guri <ignacioguri@gmail.com>
2017-09-25 16:49:16 -0300

    ABM de clientes fully functional

--- h2soft_ui/src/api/services/clientServices.js
+++ h2soft_ui/src/api/services/clientServices.js
@@ -7,6 +7,30 @@ export default {
   getClientes (context) {
     return context.$http.get(API_URL + 'clientes', authHeader)
   },
+  getCliente (context, id) {
+    return context.$http.get(API_URL + 'clientes' + '/?idClientes=' + id, authHeader)
+      .then(res => { return res.body.data })
+  },
+  getClienteFull (context, id) {
+    let info = {}
+    return context.$http.get(API_URL + 'clientes/' + id, authHeader)
+      .then(cl => {
+        info['cliente'] = cl.body
+        return context.$http.get(API_URL + 'contactos-x-cliente' + '/?idCliente=' + id, authHeader)
+      })
+      .then(cxc => {
+        info['contacto'] = cxc.body.data[0]
+        return context.$http.get(API_URL + 'objetivos-x-cliente' + '/?idCliente=' + id, authHeader)
+      })
+      .then(oxc => {
+        info['objetivos'] = oxc.body.data
+        return info
+      })
+      .catch(error => {
+        console.log('algo falló en el get ', error)
+        return false
+      })
+  },
   postClientes (context, cliente, contacto, objetivos) {
     console.log('llegue a post con: \n' + JSON.stringify(cliente) + '\n' + JSON.stringify(contacto) + '\n' + JSON.stringify(objetivos))
     return context.$http.post(API_URL + 'clientes', cliente, authHeader)
@@ -43,7 +67,7 @@ export default {
         return context.$http.delete(API_URL + 'objetivos-x-cliente/?idCliente=' + id, authHeader)
       })
       .then(() => {
-        return context.$http.delete(API_URL + 'clientes/?idClientes=' + id, authHeader)
+        return context.$http.delete(API_URL + 'clientes/' + id, authHeader)
       })
       .then(res => {
         console.log('borrado', res)
@@ -59,14 +83,36 @@ export default {
       .then(res => { return res.body.data })
   },
   getObjetivos (context, id) {
-    return context.$http.get(API_URL + 'objetivos-x-cliente/?idObjetivosXCliente=' + id, authHeader)
-      .then(res => { return res.body.data })
+    return context.$http.get(API_URL + 'objetivos-x-cliente/' + id, authHeader)
+      .then(res => { return res.body })
   },
   postObjetivos (context, objetivo) {
     return context.$http.post(API_URL + 'objetivos-x-cliente', objetivo, authHeader)
       .then(res => { return true })
       .catch(error => {
         console.log(error)
+        return false
+      })
+  },
+  editClientes (context, id, cliente, contacto, objetivos) {
+    console.log('llegue a edit con: \n' + JSON.stringify(cliente) + '\n' + JSON.stringify(contacto) + '\n' + JSON.stringify(objetivos))
+    return context.$http.patch(API_URL + 'clientes/' + id, cliente, authHeader)
+      .then(clienteUpdated => {
+        return context.$http.patch(API_URL + 'contactos-x-cliente' + '/?idCliente=' + id, contacto, authHeader)
+      })
+      .then(contactoUpdated => {
+        let promesas = []
+        objetivos.forEach(objetivo => {
+          promesas.push(context.$http.patch(API_URL + 'objetivos-x-cliente' + '/?idCliente=' + id, objetivo, authHeader))
+        })
+        return Promise.all(promesas)
+      })
+      .then(objetivosUpdated => {
+        console.log('updatee todo bien wa8')
+        return true
+      })
+      .catch(error => {
+        console.log('algo falló en el update ', error)
         return false
       })
   }
