d4fbcbf2efa03b2b4dc0bec0554721efcf11c513
emaperroud <perroudema@gmail.com>
2018-04-09 22:51:11 -0300

    Cambio de estado de dispensers al dejarlos o retirarlos de un objetivo

--- h2soft_ui/src/api/services/dispensersServices.js
+++ h2soft_ui/src/api/services/dispensersServices.js
@@ -14,14 +14,16 @@ export default {
   },
   setObjetivoADispenser (context, idDispenser, IdObjetivo) {
     var dispenser = {
-      'idObjetivo': IdObjetivo
+      'idObjetivo': IdObjetivo,
+      'idEstadoDispenser': 2 // seteo el estado "En objetivo y limpio"
     }
     return context.$http.patch(API_URL + 'dispensers/' + idDispenser, dispenser, authHeader)
       .then(res => { return res.body.data })
   },
   borrarObjetivoDeDispenser (context, idDispenser) {
     var dispenser = {
-      'idObjetivo': null
+      'idObjetivo': null,
+      'idEstadoDispenser': 4 // al retirar el dispenser del objetivo seteo el estado "En mantenimiento"
     }
     return context.$http.patch(API_URL + 'dispensers/' + idDispenser, dispenser, authHeader)
       .then(res => { return res.body.data })
