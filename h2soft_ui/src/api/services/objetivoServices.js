83a5e8fa7b7f08128161035c41533c3f9dc45383
VecchietNico <hnvecchiet@gmail.com>
2018-05-27 18:44:31 -0300

    agrego services de objetivos

--- h2soft_ui/src/api/services/objetivoServices.js
+++ h2soft_ui/src/api/services/objetivoServices.js
@@ -1,3 +1,12 @@
-/**
- * Created by apple on 27/5/18.
- */
+import auth from '../auth'
+const API_URL = process.env.API_URL
+
+export default {
+  getObjetivos (context) {
+    const authHeader = {headers: auth.getAuthHeader()}
+    return context.$http.get(API_URL + 'objetivos-x-cliente', authHeader)
+      .then(res => {
+        return res.body.data
+      })
+  }
+}
