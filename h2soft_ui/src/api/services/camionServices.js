bcd2f6699fa1b23fb430f49aeb961c00edf767bc
emaperroud <perroudema@gmail.com>
2018-05-29 01:31:26 -0300

    auth en getcamiones

--- h2soft_ui/src/api/services/camionServices.js
+++ h2soft_ui/src/api/services/camionServices.js
@@ -1,15 +1,16 @@
 import auth from '../auth'
 const API_URL = process.env.API_URL
-const authHeader = { headers: auth.getAuthHeader() }
 
 // LISTA DE TODOS LAS LLAMADAS AL SERVIDOR
 export default {
   getCamiones (context) {
+    const authHeader = { headers: auth.getAuthHeader() }
     return context.$http.get(API_URL + 'camiones', authHeader)
       .then(res => { return res.body.data })
   },
   asignarCamionARecorrido (context, idRecorridoHistorico, idCamion) {
-    var recorrido = {
+    const authHeader = { headers: auth.getAuthHeader() }
+    const recorrido = {
       'idCamionAsignado': idCamion
     }
     return context.$http.patch(API_URL + 'recorrido-historico/?idRecorridosHistoricos=' + idRecorridoHistorico, recorrido, authHeader)
