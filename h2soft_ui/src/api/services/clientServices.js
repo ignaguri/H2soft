20224ca8989a761af45fe1644959dc7db0a5a86f
Ignacio Guri <ignacioguri@gmail.com>
2018-06-03 18:04:05 -0300

    Se movió el calculo de contabilidad a client-services

--- h2soft_ui/src/api/services/clientServices.js
+++ h2soft_ui/src/api/services/clientServices.js
@@ -137,29 +137,127 @@ export default {
     return context.$http.get(API_URL + 'tipos-cliente', authHeader)
       .then(res => { return res.body.data })
   },
-  getClienteConContrato (context, id) {
+  calcularVentasPorCliente (context, cliente, fechaDesde, fechaHasta) {
     const authHeader = { headers: auth.getAuthHeader() }
-    let info = {}
-    return context.$http.get(API_URL + 'clientes/' + id, authHeader)
-      .then(cl => {
-        info['cliente'] = cl.body
-        return context.$http.get(API_URL + 'objetivos-x-cliente' + '/?idCliente=' + id, authHeader)
+    // PREPARO LAS FECHAS PARA SU USO
+    const desde = fechaDesde.split('/')
+    fechaDesde = new Date('20' + desde[2], desde[1] - 1, desde[0])
+    const hasta = fechaHasta.split('/')
+    fechaHasta = new Date('20' + hasta[2], hasta[1] - 1, hasta[0])
+    let objetivosAux
+    // BUSCO LOS OBJETIVOS DEL CLIENTE QUE VIENE POR PARAMETRO
+    return this.getObjetivos(context, cliente)
+      .then(objetivos => {
+        objetivosAux = objetivos
+        let promesas = []
+        // POR CADA OBJETIVO, QUIERO SUS REMITOS, DENTRO DEL RANGO DE FECHAS PASADAS POR PARAMETRO
+        objetivos.forEach(o => promesas.push(context.$http.get(API_URL + 'remitos/?idObjetivo=' +
+                                            o.idObjetivosXCliente + '&fecha[$gte]=' + fechaDesde.toISOString() +
+                                            '&fecha[$lt]=' + fechaHasta.toISOString(), authHeader)
+                                            .then(res => res.body.data)))
+        return Promise.all(promesas)
       })
-      .then(oxc => {
-        info['objetivos'] = oxc.body.data
-        return context.$http.get(API_URL + 'contratos' + '/?idCliente=' + id, authHeader)
+      .then(remitosXObjetivo => {
+        let promesas = []
+        // LOS REMITOS VIENEN AGRUPADOS POR OBJETIVO. POR CADA UNO DE ESOS REMITOS QUIERO SU DETALLE-REMITO-PRODUCTOS
+        // TENIENDO EN CUENTA SOLO LOS PRODUCTOS 'DEJADOS EN CLIENTE'
+        remitosXObjetivo.forEach(obj => {
+          obj.forEach(r => {
+            promesas.push(context.$http.get(API_URL + 'detalle-remito-productos/?idRemito=' +
+                                            r.idRemito + '&dejadoEnCliente=1', authHeader)
+                                          .then(rem => {
+                                            const result = {detalles: rem.body.data}
+                                            result.objetivo = r.idObjetivo
+                                            result.fecha = r.fecha
+                                            result.firmaConforme = r.firmaConforme
+                                            return result
+                                          }))
+          })
+        })
+        return Promise.all(promesas)
       })
-      .then(con => {
-        // console.log(con.body.data[0])
-        info['contrato'] = con.body.data[0]
-        return context.$http.get(API_URL + 'detalles-contrato' + '/?idContrato=' + con.body.data[0].idContratos, authHeader)
+      .then(detallesXRemito => {
+        const productos = []
+        // ESOS DETALLES SE COMPONEN DE UN ARRAY DE 'SUB'-DETALLES CORRESPONDIENTES A CADA UNO DE LOS TIPOS DE PRODUCTO
+        // Y DE INFORMACIÓN COMÚN A TODOS LOS DETALLES [POR EL REMITO] (OBJETIVO, FECHA Y SI LO FIRMÓ CONFORME)
+        detallesXRemito.forEach(dXr => {
+          dXr.detalles.forEach(d => {
+            const aux = productos.findIndex(p => p.idProducto === d.idProducto)
+            if (aux < 0) {
+              productos.push({
+                idProducto: d.idProducto,
+                cantidad: d.cantidad,
+                ventas: [
+                  {
+                    objetivo: objetivosAux.find(o => o.idObjetivosXCliente === dXr.objetivo).nombre,
+                    fecha: new Date(dXr.fecha).toLocaleDateString('es-AR', { year: '2-digit', month: '2-digit', day: '2-digit' }),
+                    cantidad: d.cantidad,
+                    firmadoconforme: dXr.firmaConforme === 1 ? 'Si' : 'No'
+                  }
+                ]
+              })
+            } else {
+              productos[aux].cantidad += d.cantidad
+              productos[aux].ventas.push({
+                objetivo: objetivosAux.find(o => o.idObjetivosXCliente === dXr.objetivo).nombre,
+                fecha: new Date(dXr.fecha).toLocaleDateString('es-AR', { year: '2-digit', month: '2-digit', day: '2-digit' }),
+                cantidad: d.cantidad,
+                firmadoconforme: dXr.firmaConforme === 1 ? 'Si' : 'No'
+              })
+            }
+          })
+        })
+        return productos
       })
-      .then(det => {
-        info['detalle'] = det.body.data
-        return info
+      .then(ventas => {
+        return this.calcularTotalPorVenta(context, cliente, ventas)
       })
       .catch(error => {
-        console.log('algo falló en el get ', error)
+        console.error('algo falló en el calculo de ventas por cliente ', error)
+        return false
+      })
+  },
+  calcularTotalPorVenta (context, cliente, ventas) {
+    const authHeader = { headers: auth.getAuthHeader() }
+    return context.$http.get(API_URL + 'contratos' + '/?idCliente=' + cliente, authHeader)
+      .then(contratos => {
+        contratos = contratos.body.data
+        const contratoVigente = contratos.find(c => c.fechaVigenciaDesde < new Date().toISOString() &&
+                                                    c.fechaVigenciaHasta > new Date().toISOString())
+        if (contratoVigente) {
+          return context.$http.get(API_URL + 'detalles-contrato/' + '?idContrato=' + contratoVigente.idContratos, authHeader)
+        } else {
+          throw new Error('No se encontraron contratos vigentes para el cliente seleccionado')
+        }
+      })
+      .then(detalles => {
+        detalles = detalles.body.data
+        const detallesPorProducto = ventas.reduce((acc, obj) => {
+          const key = obj.idProducto
+          acc[key] = detalles.filter(d => d.idProducto === key)
+          return acc
+        }, {})
+        Object.keys(detallesPorProducto).forEach(p => {
+          const venta = ventas.find(v => v.idProducto === Number(p))
+          detallesPorProducto[p].sort((a, b) => a.cantidadMaxima - b.cantidadMaxima)
+          let detalleAlQueAplica
+          detallesPorProducto[p].forEach((d, i) => {
+            if (venta.cantidad >= d.cantidadMinima && venta.cantidad <= d.cantidadMaxima) {
+              detalleAlQueAplica = d
+            }
+            if (venta.cantidad > d.cantidadMaxima) {
+              if (i === detallesPorProducto[p].length - 1) {
+                detalleAlQueAplica = d
+              }
+            }
+          })
+          venta.precioPorUnidad = detalleAlQueAplica.precioPorUnidad
+          venta.total = venta.cantidad * detalleAlQueAplica.precioPorUnidad
+        })
+        return ventas
+      })
+      .catch(error => {
+        console.error('Error en calcularTotalPorVenta', error)
         return false
       })
   }
--- h2soft_ui/src/components/Dashboard/Views/Contabilidad/Contabilidad.vue
+++ h2soft_ui/src/components/Dashboard/Views/Contabilidad/Contabilidad.vue
@@ -13,17 +13,17 @@
                 :placeholder="'Cliente'"
                 :search="true" :justified="true" style="width: 200PX;" >
             </dds>
-        </div> 
+        </div>
       </div>
       <div class="row text-line">
         <div class="col-md-3">
           <label><h4><span class="label label-default">Desde</span></h4></label>
           <dp v-model="fechaDesde" id="fechaDesde" :disabled-days-of-week=[0] :format="'dd/MM/yyyy'"  :placeholder="'Desde'" width="100%" :clear-button="true"></dp>
-        </div> 
+        </div>
         <div class="col-md-3">
           <label><h4><span class="label label-default">Hasta</span></h4></label>
           <dp v-model="fechaHasta" id="fechaHasta" :disabled-days-of-week=[0] :format="'dd/MM/yyyy'"  :placeholder="'Hasta'" width="100%" :clear-button="true"></dp>
-        </div> 
+        </div>
         <div class="col-md-3"  style="padding-top: 25px;" >
           <button type="button" class="btn btn-info btn-fill" @click="this.actualizar">Actualizar</button>
         </div>
@@ -32,7 +32,7 @@
     </form>
    <div class="row" >
       <div class="col-md-12">
-        <div class="card"> 
+        <div class="card">
           <div class="row">
                   <div class="col-md-4 text-center">
                     <h5>Precio por unidad</h5>
@@ -46,7 +46,7 @@
                     <h5>Total</h5>
                     <h4 style="margin-top: 0px;" class="">$ {{this.total}}</h4>
                   </div>
-          </div>        
+          </div>
           <paper-table type="hover" :title="table1.title" :sub-title="table1.subTitle" :data="table1.data" :columns="table1.columns" :editButton="false" :eraseButton="false" :goButton="false" >
           </paper-table>
         </div>
@@ -56,20 +56,16 @@
 </template>
 <script>
   import PaperTable from 'components/Dashboard/Views/Contabilidad/PaperTablePlusContabilidad.vue'
-  import { datepicker } from 'vue-strap'
-  import dds from 'vue-strap/src/Select.vue'
+  import { datepicker, select } from 'vue-strap'
   import api from 'src/api/services/clientServices.js'
-  import apiRemito from 'src/api/services/remitoServices.js'
   import noti from 'src/api/notificationsService'
-  // import apiDispensers from 'src/api/services/dispensersServices.js'
-  // import apiContratos from 'src/api/services/contratosServices.js'
 
   const table1Columns = ['Objetivo', 'Fecha', 'Cantidad', 'Firmado conforme']
   export default {
     components: {
       PaperTable,
       dp: datepicker,
-      dds
+      dds: select
     },
     data () {
       return {
@@ -126,59 +122,21 @@
         }
       },
       calcularValores () {
-        api.getClienteConContrato(this, this.idClientes)
-          .then(res => {
-            this.objetivos = res['objetivos']
-            this.contrato = res['contrato']
-            this.detalleContrato = res['detalle']
-            this.calcularCantidad()
-            .then(r => {
-              if (r) {
-                this.calcularVenta()
-              }
-            })
+        this.table1.data = []
+        api.calcularVentasPorCliente(this, this.idClientes, this.fechaDesde, this.fechaHasta)
+          .then(ventasXProducto => {
+            if (ventasXProducto && ventasXProducto.length) {
+              this.precioPorUnidad = ventasXProducto[0].precioPorUnidad
+              this.cantidad = ventasXProducto[0].cantidad
+              this.total = ventasXProducto[0].total
+              this.table1.data = ventasXProducto[0].ventas
+            } else {
+              this.precioPorUnidad = 0
+              this.cantidad = 0
+              this.total = 0
+              this.table1.data = []
+            }
           })
-      },
-      calcularCantidad () {
-        return new Promise((resolve, reject) => {
-          this.objetivos.forEach((obj, i) => {
-            apiRemito.getRemitoXObjetivo(this, obj.idObjetivosXCliente)
-              .then(rems => {
-                rems.forEach(rem => {
-                  let f = new Date(rem.fecha)
-                  if (f < this.hasta && f > this.desde) {
-                    apiRemito.getDetalleRemitoProducto(this, rem.idRemito)
-                    .then(dets => {
-                      dets = dets.body.data
-                      dets.forEach(det => {
-                        if (det.dejadoEnCliente === 1) {
-                          this.cantidad = this.cantidad + det.cantidad
-                          let venta = {
-                            objetivo: obj.nombre,
-                            fecha: rem.fecha === null ? '-' : new Date(rem.fecha).toLocaleDateString('es-AR', { year: '2-digit', month: '2-digit', day: '2-digit' }),
-                            cantidad: det.cantidad,
-                            firmadoconforme: rem.firmaConforme === 1 ? 'Si' : 'No'
-                          }
-                          this.table1.data.push(venta)
-                        }
-                        if (this.objetivos.length - 1 === i) { // al finalizar el for devuelvo el valor de la promise
-                          resolve(true)
-                        }
-                      })
-                    })
-                  }
-                })
-              })
-          })
-        })
-      },
-      calcularVenta () {
-        this.detalleContrato.forEach(det => {
-          if (det.cantidadMinima <= this.cantidad && this.cantidad <= det.cantidadMaxima) {
-            this.precioPorUnidad = det.precioPorUnidad
-            this.total = this.cantidad * this.precioPorUnidad
-          }
-        })
       }
     }
   }
