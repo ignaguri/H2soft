1291db8078961aa948cc3597597da207a781a47e
Ignacio Guri <ignacioguri@gmail.com>
2018-06-07 04:14:18 -0300

    Se agregó exportacion a PDF de Contratos

--- h2soft_ui/src/api/export/index.js
+++ h2soft_ui/src/api/export/index.js
@@ -1,12 +1,15 @@
 import zipcelx from 'zipcelx'
+import pdfMake from 'pdfmake/build/pdfmake'
+import pdfFonts from 'pdfmake/build/vfs_fonts'
+pdfMake.vfs = pdfFonts.pdfMake.vfs
 
 export default {
   /*
-  * exportToExcel function
-  * @title: the first line in the Excel file AND the filename
-  * @columns: [required] the columns that will go on the 1st row.
-  *   - type: Array
-  *   - example: ['Date', 'Action', 'User']
+   * exportToExcel function
+   * @title: the first line in the Excel file AND the filename
+   * @columns: [required] the columns that will go on the 1st row.
+   *   - type: Array
+   *   - example: ['Date', 'Action', 'User']
    * @rows: [required] each of the rows that will be included.
    * The order of the objects it's supposed to match the @columns
    *   - type: Array of arrays
@@ -30,8 +33,107 @@ export default {
     }
     zipcelx(config)
   },
-  exportToPDF () {
-    // not implemented yet
+  exportToPDF (data) {
+    const { razonSocial, cuil, domicilio, fechaFirma, vigenciaDesde, vigenciaHasta, clausulas } = data
+    const tablaDeProductos = [
+      [
+        {text: 'Producto', style: 'tableHeader'},
+        {text: 'Cantidad desde', style: 'tableHeader'},
+        {text: 'Cantidad hasta', style: 'tableHeader'},
+        {text: 'Precio por unidad (en ARS$)', style: 'tableHeader'}
+      ]
+    ]
+    clausulas.forEach(c => tablaDeProductos.push([c.producto, c.cantidadMinima, c.cantidadMaxima, c.precioPorUnidad]))
+    const docDefinition = {
+      info: {
+        title: 'Exported from H2Soft app',
+        author: 'H2Soft Team'
+      },
+      footer: () => {
+        const today = new Date().toLocaleString('es-AR')
+        return {text: `Impreso el ${today}`, alignment: 'right'}
+      },
+      content: [
+        {
+          text: 'Contrato con EcoAgua',
+          style: 'header',
+          alignment: 'center'
+        },
+        {
+          text: 'El siguiente documento representa un resumen del acuerdo entre las partes. No tiene validez ni formato legal de contrato.',
+          style: ['header', 'small'],
+          alignment: 'center'
+        },
+        '\n\n\n',
+        {
+          text: [
+            'Razón social del cliente: ',
+            {text: razonSocial, style: 'data'},
+            '\nCUIL: ',
+            {text: cuil, style: 'data'},
+            '\nDomicilio fiscal: ',
+            {text: domicilio, style: 'data'}
+          ],
+          style: 'normal',
+          alignment: 'left'
+        },
+        '\n\n\n',
+        {
+          text: [
+            'Fecha de firma: ',
+            {text: fechaFirma, style: 'data'},
+            '\nVigente del:  ',
+            {text: vigenciaDesde, style: 'data'},
+            '  al:  ',
+            {text: vigenciaHasta, style: 'data'}
+          ],
+          style: 'normal',
+          alignment: 'left'
+        },
+        '\n\n\n',
+        {text: 'Cláusulas', style: 'subheader', alignment: 'center'},
+        {
+          style: 'table',
+          table: {
+            headerRows: 1,
+            widths: ['*', '*', '*', '*'],
+            body: tablaDeProductos
+          }
+        }
+      ],
+      styles: {
+        header: {
+          fontSize: 24,
+          bold: true,
+          alignment: 'justify'
+        },
+        small: {
+          fontSize: 8
+        },
+        normal: {
+          fontSize: 18
+        },
+        data: {
+          italics: true,
+          bold: true,
+          fontSize: 20
+        },
+        subheader: {
+          fontSize: 16,
+          bold: true,
+          margin: [0, 10, 0, 5]
+        },
+        table: {
+          margin: [0, 5, 0, 15]
+        },
+        tableHeader: {
+          bold: true,
+          fontSize: 13,
+          color: 'black'
+        }
+      }
+    }
+    pdfMake.createPdf(docDefinition).download(`Contrato_EcoAgua_${razonSocial}`)
   }
 }
 
