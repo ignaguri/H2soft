f403f3d9a9ef8fb4fdf30b349ce9bb77f3e5ea91
VecchietNico <hnvecchiet@gmail.com>
2018-06-07 01:31:30 -0300

    ambc de camiones

---
+++ h2soft_ui/src/components/Dashboard/Views/Configuracion.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div class="row">
+      <tabs id="pestañas" v-model="this.pestañaActiva" @active="cambio" nav-style="tabs" justified>
+        <tab header="Camiones">
+          <camiones v-if="this.pestañaActiva === 0"></camiones>
+        </tab>
+      </tabs>
+    </div>
+  </div>
+</template>
+<script>
+  import { tabs, tab } from 'vue-strap'
+  import camiones from './camiones/Camiones.vue'
+
+  export default {
+    components: {
+      tabs,
+      tab,
+      camiones
+    },
+    data () {
+      return {
+        pestañaActiva: 0
+      }
+    },
+    methods: {
+    }
+  }
+</script>
---
+++ h2soft_ui/src/components/Dashboard/Views/camiones/CamionesList.vue
@@ -0,0 +1,85 @@
+<template>
+  <div>
+    <div v-if="isCamionesList">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="card">
+            <paper-table type="hover" :title="table1.title" :sub-title="table1.subTitle" :data="table1.data"
+                         :columns="table1.columns" :editButton="true" :eraseButton="true"
+                         :erase="borrarCamion" :goButton="false"
+                         :edit="editar">
+            </paper-table>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+  // import PaperTable from 'components/UIComponents/PaperTable.vue'
+  import PaperTable from 'components/UIComponents/PaperTablePlus.vue'
+  import api from 'src/api/services/camionServices'
+  import apiCliente from 'src/api/services/clientServices'
+  import noti from 'src/api/notificationsService'
+
+  const tableColumns = ['Nro', 'Cliente', 'Capacidad Máxima']
+  //  let tableData = []
+
+  export default {
+    components: {
+      PaperTable
+    },
+    data () {
+      return {
+        isCamionesList: true,
+        estados: [],
+        table1: {
+          title: 'Camiones',
+          subTitle: 'Listado de camiones de la empresa',
+          columns: [...tableColumns],
+          data: []
+        }
+      }
+    },
+    mounted () {
+      this.cargarCamiones()
+    },
+    methods: {
+      cargarCamiones () {
+        api.getCamiones(this).then(res => {
+          res.forEach(camion => {
+            this.table1.data.push({
+              nro: camion.idCamiones,
+              cliente: camion.nombre,
+              capacidadmáxima: camion.capacidadMaxima
+            })
+          })
+        }, error => {
+          console.log('error' + JSON.stringify(error))
+        })
+      },
+      editar (e) {
+        let id = e.target.parentNode.parentNode.getElementsByTagName('td')[0].innerHTML
+        this.$emit('emitted', {action: 'edit', client: id})
+        this.$parent.idCamiones = id
+      },
+      borrarCamion (e) {
+        let id = Number(e.target.parentNode.parentNode.getElementsByTagName('td')[0].innerHTML)
+        api.deleteCamion(this, id)
+          .then(res => {
+            if (res) {
+              noti.exitoConTexto(this, 'Éxito', 'El Camión se ha eliminado!')
+            }
+          })
+          .catch(err => {
+            console.log('error', err)
+            noti.errorConTexto(this, 'Error', 'Error al eliminar Camión')
+          })
+      }
+    }
+  }
+
+</script>
+<style>
+
+</style>
