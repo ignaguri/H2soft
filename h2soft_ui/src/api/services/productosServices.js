e894be701268c26dfedb6a7bc207be9afc811395
emaperroud <perroudema@gmail.com>
2018-06-26 20:58:34 -0300

    en remito aparecen solo los productos del contrato

--- h2soft_ui/src/api/services/productosServices.js
+++ h2soft_ui/src/api/services/productosServices.js
@@ -35,5 +35,26 @@ export default {
         console.log('algo falló en el update ', error)
         return false
       })
+  },
+  getProductosEnContratos (context, idObjetivo) {
+    const authHeader = { headers: auth.getAuthHeader() }
+    return context.$http.get(API_URL + 'objetivos-x-cliente' + '/?idObjetivosXCliente=' + idObjetivo, authHeader)
+  .then(objetivos => {
+    const idCliente = objetivos.body.data[0].idCliente
+    return context.$http.get(API_URL + 'contratos/?idCliente=' + idCliente, authHeader)
+  })
+  .then(contratos => {
+    contratos = contratos.body.data
+    const contratoVigente = contratos.find(c => c.fechaVigenciaDesde < new Date().toISOString() &&
+                                                 c.fechaVigenciaHasta > new Date().toISOString())
+    if (contratoVigente) {
+      return context.$http.get(API_URL + 'detalles-contrato/' + '?idContrato=' + contratoVigente.idContratos, authHeader)
+    } else {
+      throw new Error('No se encontraron contratos vigentes para el cliente seleccionado')
+    }
+  })
+  .then(detalles => {
+    return detalles.body.data.map(x => { return x.idProducto })
+  })
   }
 }
